9f802dbadcbf6b4e4d535d04f4388f678b03269b
haywoodjustin <49731705+haywoodjustin@users.noreply.github.com>
2023-02-11 11:47:19 -0500

    removed core from blob

--- LibraryRefactor/BusinessIntellegenceLibrary/BusinessExports.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef BUSINESSINTELLEGENCELIBRARY_EXPORTS
-#define BUSINESSINTELLEGENCELIBRARY_API __declspec(dllexport)
-#else
-#define BUSINESSINTELLEGENCELIBRARY_API __declspec(dllimport)
-#endif
